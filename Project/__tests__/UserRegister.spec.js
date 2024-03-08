4aa514b2d81d9e34860f9a27b1657d65afbc98e9
marboqmar <martamartinezboquer@gmail.com>
2024-03-08 16:03:29 +0100

    Dynamic tests, multiple controls for a field using bail(), and started with unique email validation

--- Project/__tests__/UserRegister.spec.js
+++ Project/__tests__/UserRegister.spec.js
@@ -73,27 +73,41 @@ describe('User Registration', () => {
     expect(body.validationErrors).not.toBeUndefined();
   });
 
-  it('returns Username cannot be null when username is null', async () => {
-    const response = await postUser({
-      username: null,
-      email: 'user1@mail.com',
-      password: 'passworD987654',
-    });
-    const body = response.body;
-    expect(body.validationErrors.username).toBe('Username cannot be null');
-  });
+  it.each`
+    field         | value              | expectedMessage
+    ${'username'} | ${null}            | ${'Username cannot be null'}
+    ${'username'} | ${'usr'}           | ${'Must have minimum 4 and maximum 32 characters'}
+    ${'username'} | ${'a'.repeat(33)}  | ${'Must have minimum 4 and maximum 32 characters'}
+    ${'email'}    | ${null}            | ${'E-mail cannot be null'}
+    ${'email'}    | ${`mail.com`}      | ${'E-mail is not valid'}
+    ${'email'}    | ${`user.mail.com`} | ${'E-mail is not valid'}
+    ${'email'}    | ${`user@mail`}     | ${'E-mail is not valid'}
+    ${'password'} | ${null}            | ${'Password cannot be null'}
+    ${'password'} | ${'P4ssw'}         | ${'Password must be at least 6 characters long'}
+    ${'password'} | ${'alllowercase'}  | ${'Password must have at least one uppercase, one lowercase and one number'}
+    ${'password'} | ${'ALLUPPERCASE'}  | ${'Password must have at least one uppercase, one lowercase and one number'}
+    ${'password'} | ${'123456789'}     | ${'Password must have at least one uppercase, one lowercase and one number'}
+    ${'password'} | ${'lowerandUPPER'} | ${'Password must have at least one uppercase, one lowercase and one number'}
+    ${'password'} | ${'lowerand123'}   | ${'Password must have at least one uppercase, one lowercase and one number'}
+    ${'password'} | ${'UPPERAND123'}   | ${'Password must have at least one uppercase, one lowercase and one number'}
+  `(
+    // Test description, which can access to the actual values of $field and $expectedMessage
+    'returns $expectedMessage when $field is $value',
+    async ({ field, value, expectedMessage }) => {
+      // The following object stores all the valid fields
+      const user = {
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'passworD987654',
+      };
+      user[field] = value;
+      const response = await postUser(user);
+      const body = response.body;
+      expect(body.validationErrors[field]).toBe(expectedMessage);
+    },
+  );
 
-  it('returns email cannot be null when email is null', async () => {
-    const response = await postUser({
-      username: 'user1',
-      email: null,
-      password: 'passworD987654',
-    });
-    const body = response.body;
-    expect(body.validationErrors.email).toBe('email cannot be null');
-  });
-
-  it('returns errors for both when username and email sare null', async () => {
+  it('returns errors for both when username and email are null', async () => {
     const response = await postUser({
       username: null,
       email: null,
@@ -101,5 +115,12 @@ describe('User Registration', () => {
     });
     const body = response.body;
     expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  it('returns E-mail in use when same email is already in use', async () => {
+    // We are going to create a user with User.create and postUser without any parameters defaults to validUser, so with this we will create two users with the same details
+    await User.create({ ...validUser });
+    const response = await postUser();
+    expect(response.body.validationErrors.email).toBe('E-mail in use');
   });
 });
--- Project/src/user/UserRouter.js
+++ Project/src/user/UserRouter.js
@@ -5,17 +5,48 @@ const { check, validationResult } = require('express-validator');
 
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null'),
-  check('email').notEmpty().withMessage('email cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    // bail means that if the previous condition (notEmpty) is not met, the bail will act as a return and the following condition will not be checked
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have minimum 4 and maximum 32 characters'),
+  check('email')
+    .notEmpty()
+    .withMessage('E-mail cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('E-mail is not valid'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long')
+    .bail()
+    .matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/)
+    .withMessage(
+      'Password must have at least one uppercase, one lowercase and one number',
+    ),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const discoveredErrors = {};
-      errors.array().forEach(error => (discoveredErrors[error.path] = error.msg)); // error.param is the key, and the message will be error.msg
+      errors
+        .array()
+        .forEach((error) => (discoveredErrors[error.path] = error.msg)); // error.param is the key, and the message will be error.msg
       return res.status(400).send({ validationErrors: discoveredErrors });
     }
-    await UserService.save(req.body);
-    return res.send({ message: 'User created' });
+    try {
+      await UserService.save(req.body);
+      return res.send({ message: 'User created' });
+    } catch (err) {
+      /*The err parameter is not used anywhere */
+      return res
+        .status(400)
+        .send({ validationErrors: { email: 'E-mail in use' } });
+    }
   },
 );
 
